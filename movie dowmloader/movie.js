923a4d2ef5962a8bb46c7d28d0cce7eec41d4c9b
Abdulmalik-Moh <abduolmalikmuhammad@gmail.com>
2025-04-13 14:14:42 +0100

    Update movie.js

--- movie dowmloader/movie.js
+++ movie dowmloader/movie.js
@@ -1,4 +1,4 @@
-// Simple JavaScript for quality selection
+
 document.querySelectorAll('.quality-btn').forEach(button => {
     button.addEventListener('click', function() {
         // Remove active class from all buttons in this quality selector
@@ -8,10 +8,4 @@ document.querySelectorAll('.quality-btn').forEach(button => {
         // Add active class to clicked button
         this.classList.add('active');
     });
-});
-
-// Search functionality would go here (would require backend integration)
-document.querySelector('.search-bar button').addEventListener('click', function() {
-    const query = document.querySelector('.search-bar input').value;
-    alert(Searching for: ${query}\n(Backend integration required));
 });
